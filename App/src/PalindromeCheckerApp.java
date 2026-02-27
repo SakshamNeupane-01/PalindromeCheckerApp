99140ce19888f4dd104577bef3f8fe3dd4024376
SakshamNeupne-01 <neupnasakcham@gmail.com>
2026-02-27 16:31:00 +0530

    welcome message

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,9 @@
+public class  PalindromeCheckerApp  {
+    public static void main(String [] args ) {
+
+    System.out.println(" welcome to PalindromeChcekcr Management System");
+    System.out.println(" version : 1.0");
+    System.out.println(" System initialize sucessfully ");
+}
+
+}
